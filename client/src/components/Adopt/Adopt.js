20f9c627b4bb4a92a26f3829fad54388658de112
Randy <83686830+ARevampedLifeCoding@users.noreply.github.com>
2021-11-30 22:25:41 -0500

    Login

--- client/src/components/Adopt/Adopt.js
+++ client/src/components/Adopt/Adopt.js
@@ -3,7 +3,7 @@ import './adopt.scss'
 export default function Adopt(){
     return(
         <div className="adopt" id="adopt">
-            <h1>Adopt page</h1>
+            <h1>Adopt page!</h1>
         </div>
     )
 }
