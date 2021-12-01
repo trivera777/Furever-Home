b2b5d450e783a081563b6260ae2c051b1e48a8c0
Randy <83686830+ARevampedLifeCoding@users.noreply.github.com>
2021-11-30 22:23:11 -0500

    Revert "login page"

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from "react";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-
-import Adopt from "./components/Adopt/Adopt";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Nav from "./components/Navbar/Navbar";
-import Menu from "./components/Menu/Menu";
-
-const httpLink = createHttpLink({
-  uri: "/graphql",
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("id_token");
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+import './App.scss';
+import { useState } from 'react';
+import Navbar from './components/Navbar/Navbar'
+import Menu from './components/Menu/Menu'
+import Adopt from './components/Adopt/Adopt';
+import Login from './components/Login/Login'
+import Signup from "./components/Signup/Signup";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <ApolloProvider client={client}>
-        <div>
-        <Router>
-          <Nav menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-          <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-          < Login />
-          < Signup />
-          <Routes>
-            <Route exact path="/" component={Adopt}  />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-          </Routes>
-          </Router>
-        </div>
-      
-    </ApolloProvider>
+<div className="app">
+    <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+    <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+    <Menu />
+    <div className="sections">
+      <Adopt />
+      <Login />
+      <Signup />
+    </div>
+</div>
   );
 }
 
--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,71 +1,99 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { Link } from 'react-router-dom';
-import { LOGIN_USER } from '../../utils/mutation';
-import Auth from '../../utils/auth';
+// import React, { useState } from 'react';
+// // import { useMutation } from '@apollo/client';
+// // import { LOGIN_USER } from '../utils/mutations';
+// import Auth from '../../utils/auth';
+import './login.scss'
 
-function Login(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
-  const [login, { error }] = useMutation(LOGIN_USER);
-
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const mutationResponse = await login({
-        variables: { email: formState.email, password: formState.password },
-      });
-      const token = mutationResponse.data.login.token;
-      Auth.login(token);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
-
+export default function Login(){
   return (
-    <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
-
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
-          </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+    <div className="login" id="login">
+      <h1>Login Page</h1>
     </div>
-  );
+  )
 }
+// const Login = (props) => {
+//   const [formState, setFormState] = useState({ email: '', password: '' });
+//   // const [login, { error, data }] = useMutation(LOGIN_USER);
 
-export default Login;
+//   const handleChange = (event) => {
+//     const { name, value } = event.target;
+
+//     setFormState({
+//       ...formState,
+//       [name]: value,
+//     });
+//   };
+
+//   const handleFormSubmit = async (event) => {
+//     event.preventDefault();
+//     console.log(formState);
+//     try {
+//       const { data } = await login({
+//         variables: { ...formState },
+//       });
+
+//       Auth.login(data.login.token);
+//     } catch (e) {
+//       console.error(e);
+//     }
+
+//     setFormState({
+//       email: '',
+//       password: '',
+//     });
+//   };
+
+//   return (
+//     <div className="login" id="login">
+//     <main className="flex-row justify-center mb-4">
+//       <div className="col-12 col-lg-10">
+//         <div className="card">
+//           <h4 className="card-header bg-dark text-light p-2">Login</h4>
+//           <div className="card-body">
+//             {data ? (
+//               <p>
+//                 Success! You may now head{' '}
+//                 <Link to="/">back to the homepage.</Link>
+//               </p>
+//             ) : (
+//               <form onSubmit={handleFormSubmit}>
+//                 <input
+//                   className="form-input"
+//                   placeholder="Your email"
+//                   name="email"
+//                   type="email"
+//                   value={formState.email}
+//                   onChange={handleChange}
+//                 />
+//                 <input
+//                   className="form-input"
+//                   placeholder="******"
+//                   name="password"
+//                   type="password"
+//                   value={formState.password}
+//                   onChange={handleChange}
+//                 />
+//                 <button
+//                   className="btn btn-block btn-primary"
+//                   style={{ cursor: 'pointer' }}
+//                   type="submit"
+//                 >
+//                   Submit
+//                 </button>
+//               </form>
+//             )}
+
+//             {error && (
+//               <div className="my-3 p-3 bg-danger text-white">
+//                 {error.message}
+//               </div>
+//             )}
+//           </div>
+//         </div>
+//       </div>
+//     </main>
+//     </div>
+//   );
+// };
+
+// export default Login;
