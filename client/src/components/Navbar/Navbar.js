853904c722644719e0ff93bfb02004873f2115cd
Randy <83686830+ARevampedLifeCoding@users.noreply.github.com>
2021-12-02 13:08:04 -0500

    Tifanny's code

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -9,6 +9,12 @@ export default function Navbar({ menuOpen, setMenuOpen }){
                 Furever Home <img src='./img/paw.gif' alt="Paw"></img>
                 </a>
             </div>
+            <div className="itemContainer">
+
+                    <div className="item">Available Cats</div>
+                    <div className="item">Available Small Pets</div>
+                    <div className="item">Success Stories ?</div>
+                </div>
             <div className="right">
                 <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
                     <span className="line1"></span>
