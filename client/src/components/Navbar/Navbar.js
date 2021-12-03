00ee846422891d37262b9092a5157a0bee7f4959
Tifanny Rivera <trivera51580@gmail.com>
2021-12-02 19:34:29 -0700

    working homepage

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -11,6 +11,7 @@ export default function Navbar({ menuOpen, setMenuOpen }){
                 Furever Home <img src='./img/paw.gif' alt="Paw"></img>
                 </a>
             </div>
+            
             {isLoggedIn && 
                 <>
                     <div>
