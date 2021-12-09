d047d988560f64c5f5f4170cd91f381035d9e6ed
satishiyer31 <satishiyer31@gmail.com>
2021-12-09 15:40:09 -0700

    confirmation page and navbar change minor

---
+++ client/src/pages/confirmation.js
@@ -0,0 +1,15 @@
+
+
+
+const Confirmation = () => {
+    return (
+        <div style= {{"color": "white", "font-size": "40px", "margin": "15px"}}>
+            Congratulations! and Thank you for Adopting from Furever Home. We hope you have wondering memories with your new pet!
+            <br></br> <br></br>
+            You will receive an email with instructions for picking up your pet! 
+        </div>
+    )
+}
+
+export default Confirmation
+
